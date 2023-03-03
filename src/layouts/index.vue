59c442b88e21e05209c84ff702ce3c50bcdee65f
Min <solunar@vip.qq.com>
2023-03-03 17:15:46 +0800

    Update

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>layouts</div>
+  <div class="fullscreen bg-dark">
+    <canvas id="particles"></canvas>
+    <div class="fullscreen absolute flex flex-center text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ipsum.</div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import Particles from 'particlesjs';
 
 export default defineComponent({
-  name: 'app-layouts'
+  name: 'app-layouts',
+
+  setup() {
+    onMounted(() => {
+      Particles.init({
+        selector: '#particles',
+        color: '#666666',
+        connectParticles: true
+      });
+    });
+  }
 });
 </script>
